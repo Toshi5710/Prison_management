ff3624c191a7cf0c23bb1eadbc4eff46b10e3b75
BadBunnyOGX <alatik59663@gmail.com>
2026-07-27 01:23:55 +0600

    Initial project setup and JavaFX FXML layouts

---
+++ src/main/java/com/example/prison_management/ChiefSecurityGuard/CellInspectionController.java
@@ -0,0 +1,7 @@
+package com.example.prison_management.ChiefSecurityGuard;
+
+public class CellInspectionController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/prison_management/MedicalDoctor/MedicalDoctor.java
@@ -0,0 +1,4 @@
+package com.example.prison_management.MedicalDoctor;
+
+public class MedicalDoctor {
+}
